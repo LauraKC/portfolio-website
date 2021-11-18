f8c7f88b457404f46056bf88b0df723f72b26be4
DeanWay <deanway93@gmail.com>
2021-11-19 00:11:26 +0100

    Updates

---
+++ component---src-pages-404-js-04d2cfe2d9b0b4bb3dfb.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunklaura_s_portfolio=self.webpackChunklaura_s_portfolio||[]).push([[883],{9616:function(e,t,o){o.r(t);var a=o(7294),n={color:"#232129",padding:"96px",fontFamily:"-apple-system, Roboto, sans-serif, serif"},l={marginTop:0,marginBottom:64,maxWidth:320};t.default=function(){return a.createElement("main",{style:n},a.createElement("title",null,"Not found"),a.createElement("h1",{style:l},"Page not found"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-04d2cfe2d9b0b4bb3dfb.js.map
