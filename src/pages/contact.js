889e6181a85fd02dbfde61a89e081d24e5ff2df0
DeanWay <deanway93@gmail.com>
2021-11-18 19:11:15 +0100

    contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,14 @@
 import React from "react"
 import Layout from "../components/layout"
+import "../styles/contact.css"
 
 const ContactPage = () => (
   <Layout>
-    <h1>
-      {"contact page!"}
-    </h1>
+    <h1><span className="purple-underline">C</span>ontact</h1>
+    <div className='contact-details'>
+      <a href="https://www.linkedin.com/in/lauracutler/"><h2>LinkedIn</h2></a>
+      <a><h2>Dribble</h2></a>
+    </div>
   </Layout>
 )
 
