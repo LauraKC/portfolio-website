c1bc05c6e8db97f6cb4a615393cb735afdea3116
DeanWay <deanway93@gmail.com>
2021-11-18 17:30:51 +0100

    underline the C

--- src/components/animated-title.js
+++ src/components/animated-title.js
@@ -3,7 +3,7 @@ import "../styles/animated-title.css"
 
 const AnimatedTitle = () => (
   <div className="animated-title">
-    <h1>Creating</h1>
+    <h1><span className="purple-underline">C</span>reating</h1>
     <h1>remarkable</h1>
     <h1>digital</h1>
     <h1>experiences</h1>
