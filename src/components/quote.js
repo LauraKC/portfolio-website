726c13d0877ec2da767a1b20c8339ccd13040946
DeanWay <deanway93@gmail.com>
2022-01-17 12:31:40 -0500

    better styling of quote component

--- src/components/quote.js
+++ src/components/quote.js
@@ -1,9 +1,11 @@
 import React from "react"
+import {quoteContainer, quoteLine, authorLine} from "./quote.module.css"
 
 const Quote = ({text, author}) => (
-    <p>
-        "{text}" -{author}
-    </p>
+  <div className={quoteContainer}>
+    <p className={quoteLine}>{text}</p>
+    <p className={authorLine}>{"~ "}{author}</p>
+  </div>
 )
 
 export default Quote
