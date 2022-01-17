ddd717201066ddd10f6c0f4a08d43dc3be85e946
DeanWay <deanway93@gmail.com>
2022-01-17 16:46:41 -0500

    fix title animation

--- src/components/animated-title.js
+++ src/components/animated-title.js
@@ -1,9 +1,10 @@
+import classNames from "classnames"
 import React from "react"
-import { animatedTitle } from "./animated-title.module.css"
+import { animatedTitle, purpleUnderline } from "./animated-title.module.css"
 
 const AnimatedTitle = () => (
   <div className={animatedTitle}>
-    <h1><span className="purple-underline">C</span>reating</h1>
+    <h1><span className={classNames("purple-underline", purpleUnderline)}>C</span>reating</h1>
     <h1>remarkable</h1>
     <h1>digital</h1>
     <h1>experiences</h1>
