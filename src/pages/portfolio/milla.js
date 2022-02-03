7ecdfec533c74ec0ba235a1a1d7ce1e154c72c38
Laura Cutler <lcutler11@gmail.com>
2022-02-03 17:02:16 +0100

    milla formatting

--- src/pages/portfolio/milla.js
+++ src/pages/portfolio/milla.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 import Quote from "../../components/quote"
+import UserFlow from "../../components/user-flow"
 import convo_screen from "../../images/milla/milla_convo_screen.png"
 import onboarding_screen from "../../images/milla/milla_onboarding_screen.png"
 import challengeimage from "../../images/milla/undraw_studying.png"
@@ -22,9 +23,17 @@ import designiteration4a from "../../images/milla/milla_design_iteration_4a.png"
 import retrospectiveimage from "../../images/milla/undraw_Conversation_re_c26v.png"
 import prototype from "../../images/milla/milla_prototype.png"
 import {
+  pageContainer,
   headerContainer,
   contentContainer,
-  goalSection
+  goalSection,
+  bulletList,
+  noBulletList,
+  quote1,
+  quote2,
+  quote3,
+  quote4,
+  flexColumns
 } from "./olive.module.css"
 import CurvedText from "../../components/curved-text"
 
@@ -93,10 +102,12 @@ const MillaPage = () => (
            <p>
              To create a mobile app based on the brief provided by CareerFoundry.
           </p>
+          <h1>The Problem Statement</h1>
+          <p>
+            {"The problem provided by CareerFoundry was 'How might we design a mobile app that empowers people to learn new vocabulary?'"}
+          </p>
         </div>
       </div>
-      <h1>The Problem Statement</h1>
-      <p>{"The problem provided by CareerFoundry was 'How might we design a mobile app that empowers people to learn new vocabulary?'"}</p>
       <h1>Understanding the Problem</h1>
       <p>
       Users need/want to be immersed in the language before they get to their travel destinations, so they can start preparing to have conversations.
@@ -142,32 +153,37 @@ const MillaPage = () => (
       />
       <h1>Proto-Persona</h1>
       <p>To understand my users point of view better, I analyzed the results of the user interviews and created a proto-persona to see the product from their eyes. This allowed me to create a product that fulfilled the users needs.</p>
-      <img className="image-right" src={protopersona} />
+      <img width="100%" src={protopersona} />
       <h1>Persona Challenges</h1>
       <p>These are quotes from user interviews that informed the challenges that Jesse faces when finding ways to be immersed in the language.</p>
       <ol>
-        <li>{"The first challenge is understanding structures of sentences and explanations of word use. Because there isn't always clear information on these items during his learning process often he has to navigate away from his learning to look up things."}</li>
+        <li>{"The first challenge is understanding structures of sentences and explanations of word use. Because there isn't always clear information on these items during his learning process often he has to navigate away from his learning to look up things."}
+        </li>
       </ol>
       <Quote 
         text="I seek out explanations on why certain words are used. Not just direct translations but explanations around how words are used."
+        author="Participant 1 (27)"
       />
       <ol>
         <li>Secondly, feeling comfortable speaking the language.</li>
       </ol>
       <Quote 
         text="When speaking to a native speaker I feel nervous and mostly that…it's probably very obvious that I don't speak the language."
+        author="Participant 4 (27)"
       />
       <ol>
         <li>Difficulty finding examples where he can listen to local speech.</li>
       </ol>
       <Quote 
         text="An app that has voices of real people saying phrases would be helpful so that you can get used to how words actually sound in a local accent rather than an AI's pronunciation."
+        author="Participant 2 (33)"
       />
       <ol>
         <li>Jesse is busy with arranging things for his move. He needs an app that can help him learn Dutch easily and in a flexible way, at times that are convenient to him.</li>
       </ol>
       <Quote 
         text=" I study in short sessions that I can squeeze in around the other things I have to do in a day."
+        author="Participant 3 (25)"
       />
       <h1>The Problem Statement</h1>
       <p>Jesse <b>needs a way</b> to be immersed in the language <b>because</b> constant exposure to the language is the most effective way to learn.</p>
@@ -216,8 +232,8 @@ const MillaPage = () => (
       <h1>Early Prototypes Iterations</h1>
       <p>{"At first the lessons would finish and go straight back to the lessons page. The abruptness of the change left users confused and feeling like the lesson was incomplete. Having added a success screen after the lesson, not only allowed for a smoother transition for the user but also allowed for some gamification aspects to be shown and a place to motivate the user, and help them embrace mistakes (they're going to happen!) with exciting images and congratulatory wording."}</p>
       <p>Another big issue was updating the navigation. At first I just used text as I was showing a low-fidelity prototype to reduce feedback on design however, the text ended up being a problem as it was too small to read. I updated the navigation to have icons and all users were instantly able to understand those better.</p>
-      <img className="image-left" src={iteration1} />
-      <img className="image-right" src={iteration2} />
+        <img className="image-left" width="50%" src={iteration1} />
+        <img className="image-right" width="50%" src={iteration2} />
       <h1>{"Usability Test Plan & Script"}</h1>
       <p>To test the MILLA prototype I created a usability test plan that asked 4 users that were either currently expats or frequent travellers to complete the following 4 direct tasks.</p>
       <p>Read the full <a href="https://drive.google.com/file/d/1UibnHX7BjybTPz-591oKU-_B0CfJTGu9/view?usp=sharing">Usability Test Plan</a>(PDF) which includes scope, schedule, metrics, script, and participant notes.</p>
@@ -228,7 +244,7 @@ const MillaPage = () => (
       <p>2 = Minor usability problem: fixing this should be given low priority.</p>
       <p>3 = Major usability problem: important to fix and should be given high priority.</p>
       <p>4 = Usability catastrophe: imperative to fix before product can be released.</p>
-      <img src={testreport} />
+      <img width="100%" src={testreport} />
       <h1>Design Iterations</h1>
       <p>Using my usability testing report I made some iterations to my designs by taking the most severe issues and finding solutions to the problems.</p>
       <p><b>Observation #1:</b></p>
@@ -237,54 +253,66 @@ const MillaPage = () => (
       <p>I greyed out the video so it seemed less interactive and more representative of the types of content they can expect in the app, as well I updated the explaination of personalization.</p>
       <p><b>Value:</b></p>
       <p>Users were less confused and able to easily understand what the application will include.</p>
-      <Quote text="Was a little confused by camera and video because was wondering if it would be a demonstration not just an image."/>
-      <h3>Before</h3><h3>After</h3>
-      <img className="image-left" src={designiteration1b} />
-      <img className="image-right" src={designiteration1a} />
+      <Quote 
+        text="Was a little confused by camera and video because was wondering if it would be a demonstration not just an image."
+        author="Rachel (32)"
+      />
+      <h3 className="image-left">Before</h3><h3>After</h3>
+      <img className="image-left" width="50%" src={designiteration1b} />
+      <img className="image-right" width="50%" src={designiteration1a} />
       <p><b>Observation #2:</b></p>
       <p>{"Most users were confused by the navigation (camera, circle button, audio) on the explore screen. Users were also concerned that the item wasn't being added to their lesson and would use different methods to check."}</p>
       <p><b>Improvements:</b></p>
       <p>{"I re-designed the buttons to what is heavily recognized and understood in the iPhone camera app, which is the swipe to switch modes. I also, changed the users involvement in adding the item to the lesson by automatically adding the item and providing a popup letting them know that the item has been added. That way they don't have to worry about their involvement in making sure the item has been added."}</p>
       <p><b>Value:</b></p>
       <p>Users are more easily able to use the explore function. The buttons are more clear and the app will add the item to their lessons easily.</p>
-      <Quote text="I thought the button in the middle was to take the picture but then I thought that's what the camera would be so maybe the camera is a library?"/>
+      <Quote 
+        text="I thought the button in the middle was to take the picture but then I thought that's what the camera would be so maybe the camera is a library?"
+        author="Darwin (27)"
+      />
       <h3>Before</h3><h3>After</h3>
-      <img className="image-left" src={designiteration2b} />
-      <img className="image-right" src={designiteration2a} />
+      <img className="image-left" width="50%" src={designiteration2b} />
+      <img className="image-right" width="50%" src={designiteration2a} />
       <p><b>Observation #3:</b></p>
       <p>Some users were unsure how to close the grammar box once it was open.</p>
       <p><b>Improvements:</b></p>
       <p>{"I re-designed this by greying out the background so users would know that they can click anywhere out of the grammar box to close it. I decided against an 'X' because I didn't want the user to be limited to clicking only one spot on the screen to close the box."}</p>
       <p><b>Value:</b></p>
       <p>The interaction is easier for users to understand but also the focus is now fully on the popup box which makes this information easier for the users to see.</p>
-      <Quote text="I am confused how to close the box, I'll just keep clicking around until it goes away."/>
+      <Quote 
+        text="I am confused how to close the box, I'll just keep clicking around until it goes away."
+        author="Andrew (25)"
+      />
       <h3>Before</h3><h3>After</h3>
-      <img className="image-left" src={designiteration3b} />
-      <img className="image-right" src={designiteration3a} />
+      <img className="image-left" width="50%" src={designiteration3b} />
+      <img className="image-right" width="50%" src={designiteration3a} />
       <p><b>Observation #4:</b></p>
       <p>All users were confused by the terms that were used in this feature. One user was also unsure if they would ever user the feature as it was presented.</p>
       <p><b>Improvements:</b></p>
       <p>{"Based on usability testing this iteration included changing most of the terms! 'Canvas' became 'Make Connections' to clarify to the users what would happen in the space. To make it extra clear how to use this space I added description text to explain how to use this feature. To make the notes feature easier to use I included it in the lessons as this is the point when most users would want to make a note about something they have just learned. That way they don't have to try and remember it after their lesson or exit out of the lesson just to make a note. Of course they can always review their notes by going into the notes page."}</p>
       <p><b>Value:</b></p>
       <p>This re-design makes the feature easier for the user to access as well as making this feature more easily understood by the user.</p>
-      <Quote text="I don't think I would use the notes function. If I'm exploring the app before doing a lesson, I wouldn't have anything to add yet and I would hesitate to ever use this feature."/>
+      <Quote 
+        text="I don't think I would use the notes function. If I'm exploring the app before doing a lesson, I wouldn't have anything to add yet and I would hesitate to ever use this feature."
+        author="Rachel (32)"
+      />
       <h3>Before</h3><h3>After</h3>
-      <img className="image-left" src={designiteration4b} />
-      <img className="image-right" src={designiteration4a} />
+        <img className="image-left" src={designiteration4b} />
+        <img className="image-right" src={designiteration4a} />
       <h1>Final Prototype</h1>
       <p>Having gone through a few more design iterations I came up with my final design prototype.</p>
-      <a href="https://marvelapp.com/prototype/14e28chj/screen/78699185"><img src={prototype} /></a>
+      <a href="https://marvelapp.com/prototype/14e28chj/screen/78699185" width="100%" className="image-center"><img src={prototype} /></a>
       <h1>Summary</h1>
       <p>{"MILLA solves the problem of 'how might we design a mobile app that empowers people to learn new vocabulary' by providing travellers and expats with a way to be immersed in a new language and gain exposure to the language. Through exploring their surroundings, listening to various audio clips and completing many different types of language lessons."}</p>
       <p>The next iterations I would like to take are to continue improving on the notes feature, include a MILLA assistant feature (a chat bot that users could interact with throughout the app to ask questions and enhance their understanding of the language) and of course I would like to create a design system and high-fidelity prototype to test with users.</p>
       <p>My hope is that when users are using MILLA that they feel empowered to keep learning and practicing new languages so that they can feel more comfortable speaking to native speakers.</p>
-      <img className="image-left" src={retrospectiveimage} />
       <h1>Retrospective:</h1>
       <p>My original intention was to design a mobile app for travellers and expats that empower them to learn a new language.</p>
       <p>I am most proud of the explore feature as I feel that the usability iterations made were the most helpful. As well, I think this particular feature can be really helpful for putting sentences together on your own. </p>
       <p>My biggest challenge with this project was trying to create something that was different from the products already on the market. Although, MILLA has a similar structure to other language learning apps I think I was able to accomplish a unique aspect by focusing on immersing the user as much as possible into the language they are learning.</p>
       <p>I really enjoyed analyzing user interviews. I found it really interesting that although they all had different language learning stories and goals they all felt similarily about their language learning processes.</p>
       <p>{"I learned that UX copy is super important and can be the difference between a clear, easy to understand feature and a confusing feature that won't be utilized by the user."}</p>
+      <img width="100%" className="image-left" src={retrospectiveimage} />
     </div>
   </div>
 )
