5ba9948e757b4672c737aac11cc7d5d1ba4b139e
DeanWay <deanway93@gmail.com>
2022-02-04 12:46:38 -0500

    improve portfolio index page layout

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -2,21 +2,30 @@ import classNames from "classnames"
 import { Link } from "gatsby"
 import React from "react"
 import Layout from "../../components/layout"
-import "../../styles/portfolio.css"
 import iphoneImage from  "../../images/olive/iphone-welcome.png"
 import millaiphoneImage from  "../../images/milla/milla_convo_screen.png"
 import treeImage from  "../../images/olive/tree.svg"
 import ContactMe from "../../components/contact-me"
+import {
+  projectContentContainer,
+  projectButtonsContainer,
+  projectButton,
+  projectButtonOlive,
+  projectButtonMilla,
+  projectButtonComingsoon,
+  oliveIphoneImage,
+  oliveTreeImage,
+} from "./index.module.css"
 
 
 const PortfolioPage = () => (
   <Layout pageContentClassName={"hello"}>
-    <div className='portfolio-content-container'>
+    <div className={projectContentContainer}>
       <h1>
         <span className='purple-underline'>P</span>ortfolio
       </h1>
 
-      <div className="project-buttons-container" style={{paddingBottom: "50px"}}>
+      <div className={projectButtonsContainer} style={{paddingBottom: "50px"}}>
         <OilveCard/>
         <MillaCard/>
         <ComingSoonCard/>
@@ -29,26 +38,26 @@ const PortfolioPage = () => (
 const OilveCard = () => (
   <Link
     to='/portfolio/olive'
-    className={classNames("project-button", "project-button-olive")}
+    className={classNames(projectButton, projectButtonOlive)}
   >
     <h1>Olive</h1>
-    <img className='olive-iphone-image' height="60%" src={iphoneImage}/>
-    <img className='olive-tree-image' height="60%" src={treeImage}/>
+    <img className={oliveIphoneImage} height="60%" src={iphoneImage}/>
+    <img className={oliveTreeImage} height="60%" src={treeImage}/>
   </Link>
 )
 
 const MillaCard = () => (
-  <Link  
+  <Link
     to='/portfolio/milla'
-    className={classNames("project-button", "project-button-milla")}
+    className={classNames(projectButton, projectButtonMilla)}
   >
     <h1>MILLA</h1>
-    <img className='milla-iphone-image' height="60%" src={millaiphoneImage}/>
+    <img height="60%" src={millaiphoneImage}/>
   </Link>
 )
 
 const ComingSoonCard = () => (
-  <Link className={classNames("project-button", "project-button-comingsoon")}>
+  <Link className={classNames(projectButton, projectButtonComingsoon)}>
     <h1>Coming Soon</h1>
   </Link>
 )
