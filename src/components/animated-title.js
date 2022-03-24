fd7b3f8bd8362785d2aa953273c3adc01be4705a
Laura Cutler <lcutler11@gmail.com>
2022-03-25 00:02:58 +0100

    fix nav link clickability

--- src/components/animated-title.js
+++ src/components/animated-title.js
@@ -4,7 +4,7 @@ import { animatedTitle, purpleUnderline } from "./animated-title.module.css"
 
 const AnimatedTitle = () => (
   <div className={animatedTitle}>
-    <h1><span className={classNames("purple-underline", purpleUnderline)}>E</span>nthusiastic</h1>
+    <h1 style={{whiteSpace: "nowrap"}} ><span className={classNames("purple-underline", purpleUnderline)}>E</span>nthusiastic</h1>
     <h1>product</h1>
     <h1>designer.</h1>
   </div>
